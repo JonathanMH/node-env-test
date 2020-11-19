8b67ea80ad84c4809a5dc82773b0b66a659a9460
Jonathan M. Hethey <public@jonathanmh.com>
2020-11-19 02:04:49 +0100

    fixes variable interpolation

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-rc='' # remote command
-rc='echo one $ANIMAL' # remote command
-rc="echo two $ANIMAL" # remote command
+rc="" # remote command
 # get login token OR
-rc+='aws ecr get-login-password --region eu-central-1 | '
+rc+="aws ecr get-login-password | "
 # get login to docker registry AND
-rc+='docker login --username AWS --password-stdin $AWS_ECR_ACCOUNT_URL && '
-# stop and remove docker image, don't check for success, could be the first time
-rc+='docker stop $AWS_ECR_REPO; docker rm $AWS_ECR_REPO; '
+rc+="docker login --username AWS --password-stdin $AWS_ECR_ACCOUNT_URL && "
+# stop and remove docker image, don"t check for success, could be the first time
+rc+="docker stop $AWS_ECR_REPO; docker rm $AWS_ECR_REPO; "
 # pull updated image AND
-rc+='docker pull $AWS_ECR_ACCOUNT_URL/$AWS_ECR_REPO && '
+rc+="docker pull $AWS_ECR_ACCOUNT_URL/$AWS_ECR_REPO && "
 # run image with project name, exposing port, passing env variable
-rc+='docker run --name=$AWS_ECR_REPO --env ANIMAL=$ANIMAL --restart=always --detach -p 3000:3000 $AWS_ECR_ACCOUNT_URL/$AWS_ECR_REPO:latest'
+rc+="docker run --name=$AWS_ECR_REPO --env ANIMAL=$ANIMAL --restart=always --detach -p 3000:3000 $AWS_ECR_ACCOUNT_URL/$AWS_ECR_REPO:latest"
 
 ssh -o StrictHostKeyChecking=no \
 -o SendEnv="ANIMAL" \
